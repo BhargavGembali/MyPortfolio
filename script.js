d1ad774935accdbd99b4d2eb58458586aa76e039
Bhargav Gembali <113098503+BhargavGembali@users.noreply.github.com>
2024-03-11 17:00:10 +0530

    some changes in the layout and some features

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
+const menu_btn= document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+
+menu_btn.addEventListener('click', function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var mediaScroller = document.querySelector('.media-scroller');
     if (mediaScroller) {
@@ -43,3 +53,4 @@ window.addEventListener("load", () => {
         });
     }, 1000); // 4 seconds delay (4000 milliseconds)
 });
+
